e94adb1bdd9d610553bd6e87d4f82b395e351d58
shadowforce78 <planque.adam@gmail.com>
2024-09-22 21:35:42 +0200

    feat: Add slash command to fetch user's timetable
    This commit adds a new slash command named "edt" to fetch the timetable of the user's class in the Discord bot. The command retrieves the timetable data from the UVSQ website using an HTTP POST request and displays it in an embedded message. The timetable includes the course name, professor's name, room number, start date, and end date for each event.
    
    Dependencies are also updated to include the "axios" package, which is used for making the HTTP request.
    
    Note: The commit message has been modified to align with the established commit message conventions in the repository.

---
+++ SlashCommands/uvsq/getedt.js
@@ -0,0 +1,71 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const schemaClasse = require('../../schema/classe');
+const axios = require('axios');
+
+module.exports = {
+    name: "edt",
+    description: "Permet de définir la classe de l'utilisateur",
+    userperm: [""],
+    botperm: [""],
+    /**
+    *
+    * @param {Client} client
+    * @param {CommandInteraction} interaction
+    * @param {String[]} args
+    */
+    run: async (client, interaction, args) => {
+
+        const classeUser = interaction.options.getString("classe");
+        const url = "https://edt.iut-velizy.uvsq.fr/Home/GetCalendarData";
+        const postdata = "start=2024-09-24&end=2024-09-24&resType=103&calView=agendaWeek&federationIds%5B%5D=INF1-B&colourScheme=3";
+        const method = "POST";
+
+        const headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Accept": "application/json, text/javascript, */*; q=0.01"
+        };
+
+        const response = await axios.post(url, postdata, { headers: headers });
+
+        const events = response.data; // Pas besoin de JSON.stringify ici, data est déjà un tableau d'objets
+
+        const cours = [];
+
+        events.forEach(event => {
+            const nomCours = event.eventCategory; // Nom du cours
+            const salleCours = event.sites[0];    // Salle du cours (première entrée dans "sites")
+
+            // Extraction du nom du prof depuis "description"
+            const descriptionParts = event.description.split('<br />');
+            const nomProf = descriptionParts[0].trim(); // Le nom du prof est avant le premier <br />
+
+            const dateDebut = event.start; // Date de début du cours
+            const dateFin = event.end;
+
+            cours.push({
+                nomCours,
+                salleCours,
+                nomProf,
+                dateDebut,
+                dateFin
+            });
+        });
+
+        const embed = new MessageEmbed()
+            .setTitle("Emploi du temps")
+            .setColor("#FF8080")
+            .setFooter("UVSQ")
+            .setTimestamp()
+            .setDescription("Voici votre emploi du temps")
+            .addFields(
+                cours.map(cours => {
+                    return {
+                        name: cours.nomCours,
+                        value: `Professeur : ${cours.nomProf}\nSalle : ${cours.salleCours}\nDate de début : ${cours.dateDebut}\nDate de fin : ${cours.dateFin}`
+                    };
+                })
+            );
+
+        interaction.followUp({ embeds: [embed] });
+    }
+};
