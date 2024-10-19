52ea3d7a0a4985193ec8234d3a98b297b7e7e0a4
shadowforce78 <planque.adam@gmail.com>
2024-10-19 18:29:46 +0200

    Refactor setclasse.js to use JSON file for user data storage

--- SlashCommands/uvsq/test.js
+++ SlashCommands/uvsq/test.js
@@ -1,5 +1,6 @@
 const { Client, CommandInteraction } = require("discord.js");
-const db = require("../../db.json");
+const fs = require("fs");
+const classeDB = require('../../db.json')
 
 module.exports = {
     name: "test",
@@ -15,26 +16,46 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
 
-        if (interaction.user.id != "918916801994309752") {
-            return interaction.followUp({
-                content: "Vous n'avez pas la permission d'utiliser cette commande (seul le developpeur peut l'utiliser)",
-            });
+        // if (interaction.user.id != "918916801994309752") {
+        //     return interaction.followUp({
+        //         content: "Vous n'avez pas la permission d'utiliser cette commande (seul le developpeur peut l'utiliser)",
+        //     });
+        // }
+
+        // Ajout de l'id de l'utilisateur dans la base de données
+        let user = interaction.user.id
+        let userDB = classeDB[user]
+        // Vérifier si l'utilisateur existe déjà dans la base de données
+        if (!classeDB[user]) {
+            // Si l'utilisateur n'existe pas, on le crée
+            classeDB[user] = {
+                id: user,
+                classe: "INF2-B1",
+                dailyReminder: false,
+                weeklyReminder: false,
+            };
+            console.log("Nouvel utilisateur ajouté :", classeDB[user]);
+        } else {
+            // Si l'utilisateur existe, on met à jour ses informations
+            classeDB[user].classe = "INF2-B1";  // Mettre à jour la classe (exemple)
+            classeDB[user].dailyReminder = false;  // Mettre à jour dailyReminder (exemple)
+            classeDB[user].weeklyReminder = false;  // Mettre à jour weeklyReminder (exemple)
+            console.log("Informations utilisateur mises à jour :", classeDB[user]);
         }
 
-        const dbSchema = {
-            id: String,
-            classe: String,
-            dailyReminder: Boolean,
-            weeklyReminder: Boolean,
-        }
+        // Sauvegarder les modifications dans le fichier JSON
+        fs.writeFile('./db.json', JSON.stringify(classeDB, null, 4), (err) => {
+            if (err) {
+                console.log("Erreur lors de la sauvegarde :", err);
+            } else {
+                console.log("Données sauvegardées avec succès !");
+            }
+        });
 
-        const id = interaction.user.id;
-        const classe = "ClasseTest";
-        const dailyReminder = false;
-        const weeklyReminder = false;
 
-        db.push({ id: id, classe: classe, dailyReminder: dailyReminder, weeklyReminder: weeklyReminder });
-        interaction.followUp({ content: "Utilisateur ajouté à la base de données" });
+        interaction.followUp({
+            content: "Commande de test effectuée avec succès !"
+        })
 
     },
 };
